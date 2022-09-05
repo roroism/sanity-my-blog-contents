90586494182fd6e5e43e96da26c267ee2544a232
roroism <miroiro0303@gmail.com>
2022-09-05 15:55:50 +0900

    feat: create sanity project

---
+++ components/CodeInput.jsx
@@ -0,0 +1,51 @@
+import React, { useCallback } from "react";
+import { FormField } from "@sanity/base/components";
+import AceEditor from "react-ace";
+import PatchEvent, { set, unset } from "@sanity/form-builder/PatchEvent";
+
+import "ace-builds/src-noconflict/theme-github";
+import "ace-builds/src-noconflict/mode-javascript";
+
+const CodeInput = React.forwardRef((props, ref) => {
+  const {
+    type, // Schema information
+    value, // Current field value
+    markers, // Markers including validation rules
+    presence, // Presence information for collaborative avatars
+    onChange,
+  } = props;
+
+  const codeChange = useCallback(
+    (code) => {
+      onChange(PatchEvent.from(code ? set(code) : unset()));
+    },
+    [onChange]
+  );
+
+  return (
+    <FormField
+      description={type.description} // Creates description from schema
+      title={type.title} // Creates label from schema title
+      __unstable_markers={markers} // Handles all markers including validation
+      __unstable_presence={presence} // Handles presence avatars
+    >
+      <AceEditor
+        mode="javascript"
+        name="ace-editor-code"
+        width="100%"
+        theme="github"
+        style={{
+          boxShadow: "0 0 0 1px #cad1dc",
+          lineHeight: 1.6,
+        }}
+        value={value}
+        tabSize={2}
+        setOptions={{ useWorker: false }}
+        ref={ref}
+        onChange={codeChange}
+      />
+    </FormField>
+  );
+});
+
+export default CodeInput;
